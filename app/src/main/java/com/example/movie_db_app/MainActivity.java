0e63bc5e425d80c3d09e3a9cb7023fce2ad1d499
benjemia <benjemia@mobiblanc.com>
2020-09-02 13:59:57 +0100

    initial commit

---
+++ app/src/main/java/com/example/movie_db_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.movie_db_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
